431bcc314bdbe5d393377471dba1befe7befb3a6
SantiB17 <perrochato08@gmail.com>
2020-01-11 00:01:53 -0800

    Setting up Firebase

--- app/src/main/java/com/santi/bearhacks/MainActivity.java
+++ app/src/main/java/com/santi/bearhacks/MainActivity.java
@@ -2,13 +2,33 @@ package com.santi.bearhacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText enterName;
+    private EditText enterAge;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterName = findViewById(R.id.user_name);
+        enterAge = findViewById(R.id.user_age);
+        nextButton = findViewById(R.id.next_button);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Intent intent = new Intent(MainActivity.this, LogData.class);
+            }
+        });
+
+
     }
 }
